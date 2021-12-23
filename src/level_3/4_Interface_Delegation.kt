69038be20ba7f115645c326b916f02a2a725c591
HarryPark <jungsanmango@gmail.com>
2021-12-23 12:26:00 +0900

    Typo Fix

--- src/level_3/4_Interface_Delegation.kt
+++ src/level_3/4_Interface_Delegation.kt
@@ -28,7 +28,7 @@ class UserRepositoryImp : UserRepository {
 
 // 생성자로 주입받은 userRepository 에 delegate 하면서 UserViewModel 은 주입받은 객체를 덮어쓰게 됩니다.
 // 그래서 UserRepository 를 상속 받았지만, 주입받은 UserRepository 의 구현을 전부 위임받아 UserRepository 내부의 구현을 처리할 필요가 없습니다.
-// 즉 그 해당 객체의 구현을 위임하기 때문에 동일한 구현을 다른 클래스 내에 여러번 하지 않아도 되는 이점으로
+// 즉 그 해당 객체의 구현을 위임하기 때문에 동일한 구현을 다른 클래스 내에 여러번 하지 않아도 되는 이점을 가지고 있습니다.
 class UserViewModel(userRepository: UserRepository) : UserRepository by userRepository
 
 fun main() {
